5ae3d3b70aa6fd88dce95631e3d31dbb14354fd0
philip.chen <philip.chen@eiptv.net>
2021-06-08 15:50:35 +0800

    更新
    拿掉JS

--- ad/test/checkText.js
+++ ad/test/checkText.js
@@ -1,9 +1,15 @@
-// 只能输入中文,英文
-function checkChinese(e) {
-	value = $(e).val();
-	value = value.replace(/[^\u4E00-\u9FA5A-Za-z]/g,'');
-	$(e).val(value);
+function chText()
+{
+    var str=document.getElementById("lastname");
+    var regex=/[^a-z]/gi;
+    str.value=str.value.replace(regex ,"");
 }
+// 只能输入中文,英文
+// function checkChinese(e) {
+// 	value = $(e).val();
+// 	value = value.replace(/[^\u4e00-\u9fa5]/g,'');
+// 	$(e).val(value);
+// }
 
 // 只能输入英文,数字
 // function checkEnglish(e) {
