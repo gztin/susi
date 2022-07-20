666f62993b3bba20d49564dfa9e744e2b5230b0a
PhilipChenET <atharsfake@gmail.com>
2022-07-20 17:07:08 +0800

    更新
    修正倒數時間的bug

--- 0717project/js/time.js
+++ 0717project/js/time.js
@@ -1,40 +1,49 @@
 
-  var DifferenceHour = -1
-  var DifferenceMinute = -1
-  var DifferenceSecond = -1
-  var Tday = new Date("July 31, 2022 23:59:59") 
-  var daysms = 24 * 60 * 60 * 1000
-  var hoursms = 60 * 60 * 1000
-  var Secondms = 60 * 1000
-  var microsecond = 1000
+  // var DifferenceHour = -1
+  // var DifferenceMinute = -1
+  // var DifferenceSecond = -1
+  // var Tday = new Date("July 31, 2022 23:59:59") 
+  // var daysms = 24 * 60 * 60 * 1000
+  // var hoursms = 60 * 60 * 1000
+  // var Secondms = 60 * 1000
+  // var microsecond = 1000
 
 function clock()
 {
-  var time = new Date()
-  var hour = time.getHours()
-  var minute = time.getMinutes()
-  var second = time.getSeconds()
-  var timevalue = ""+((hour > 12) ? hour-12:hour)
-  timevalue +=((minute < 10) ? ":0":":")+minute
-  timevalue +=((second < 10) ? ":0":":")+second
-  timevalue +=((hour >12 ) ? " PM":" AM")
-//  document.formnow.now.value = timevalue
-  var convertHour = DifferenceHour
-  var convertMinute = DifferenceMinute
-  var convertSecond = DifferenceSecond
-  var Diffms = Tday.getTime() - time.getTime()
-  DifferenceHour = Math.floor(Diffms / daysms)
-  Diffms -= DifferenceHour * daysms
-  DifferenceMinute = Math.floor(Diffms / hoursms)
-  Diffms -= DifferenceMinute * hoursms
-  DifferenceSecond = Math.floor(Diffms / Secondms)
-  Diffms -= DifferenceSecond * Secondms
-  var dSecs = Math.floor(Diffms / microsecond)
-  if(convertHour != DifferenceHour) document.formnow.dd.value=DifferenceHour
-  if(convertMinute != DifferenceMinute) document.formnow.hh.value=DifferenceMinute
-  if(convertSecond != DifferenceSecond) document.formnow.mm.value=DifferenceSecond
-  document.formnow.ss.value=dSecs
-//  document.formnow.Tnow.value= DifferenceHour  DifferenceMinute + DifferenceSecond + dSecs
-  setTimeout("clock()",1000)
+  var timer=null;
+  var show=document.getElementById("show");
+  function show_date_time(){   
+    var target=new Date("2022/7/21");  
+    var today=new Date(); 
+    var timeold=(target.getTime()-today.getTime());   
+    var sectimeold=timeold/1000   
+    var secondsold=Math.floor(sectimeold);   
+    var msPerDay=24*60*60*1000   
+    var e_daysold=timeold/msPerDay   
+    var daysold=Math.floor(e_daysold);   
+    var e_hrsold=(e_daysold-daysold)*24;   
+    var hrsold=Math.floor(e_hrsold);   
+    var e_minsold=(e_hrsold-hrsold)*60;   
+    var minsold=Math.floor((e_hrsold-hrsold)*60);   
+    var seconds=Math.floor((e_minsold-minsold)*60);
+    if(daysold<0){   
+      document.getElementById("time").innerHTML="逾期,倒數計時已經失效";  
+      clearInterval(timer);
+    }   
+    else{   
+      if (daysold<10) {daysold="0"+daysold}   
+      if (hrsold<10) {hrsold="0"+hrsold}   
+      if (minsold<10) {minsold="0"+minsold}   
+      if (seconds<10) {seconds="0"+seconds}
+      
+      $(".dd").val(daysold);
+			$(".hh").val(hrsold);
+			$(".mm").val(minsold);
+			$(".ss").val(seconds);
+
+      // show.innerHTML="距離結束時間還有:"+daysold+"天"+hrsold+"小時"+minsold+"分"+seconds+"秒";     
+    }   
+  }   
+  timer=setInterval(show_date_time,1000);
 } 
 
