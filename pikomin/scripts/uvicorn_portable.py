e19603953a4fbb5caf286605a9e6e95f8972c5e6
gztin <atharsfake@gmail.com>
2026-08-06 15:11:29 +0800

    更新
    專案搬家

--- pikomin/scripts/uvicorn_portable.py
+++
@@ -1,9 +0,0 @@
-from importlib.metadata import entry_points
-
-
-def main() -> int:
-    return entry_points(group="console_scripts")["uvicorn"].load()()
-
-
-if __name__ == "__main__":
-    raise SystemExit(main())
